6cd24db7f3ff79ee1dab1244c2ad497cdb22bd16
idr3w <ibandaandrew8@gmail.com>
2021-10-01 11:37:01 +0300

    update ireporter

--- src/main/java/org/pahappa/systems/controllers/IncidentServiceImplementation.java
+++ src/main/java/org/pahappa/systems/controllers/IncidentServiceImplementation.java
@@ -1,4 +1,199 @@
 package org.pahappa.systems.controllers;
 
-public class IncidentServiceImplementation {
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.pahappa.systems.views.HibernateUtil;
+import org.pahappa.systems.enums.Status;
+import org.pahappa.systems.exceptions.ValidationFailedException;
+import org.pahappa.systems.models.Incident;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional
+public class IncidentServiceImplementation implements IncidentService {
+	private static Transaction transObj;
+	private static final Session sessionObj = HibernateUtil.getSessionFactory().openSession();
+
+	private static int incidentIds=0;
+
+	@Override
+	public void saveIncident(Incident incident) throws Exception {
+
+		if (incident.getTitle()==null){
+			throw new ValidationFailedException("Please enter title");
+		}
+		if (incident.getTitle().isEmpty()){
+			throw new ValidationFailedException("Please enter the title");
+		}
+		if (incident.getComment()==null){
+			throw new ValidationFailedException("Please enter comment");
+		}
+		if (incident.getComment().isEmpty()){
+			throw new ValidationFailedException("please enter comment.");
+		}
+		try {
+
+			incident.setId(++incidentIds);
+			incident.setStatus(Status.DRAFT);
+			incident.setCreatedOn( new Date());
+
+			transObj = sessionObj.beginTransaction();
+			sessionObj.save(incident);
+			System.out.println("Record saved...");
+
+		} catch (HibernateException exceptionObj) {
+			exceptionObj.printStackTrace();
+		} finally {
+			transObj.commit();
+		}
+	}
+
+	@Override
+	public void updateIncident(Incident incident) throws Exception {
+		try {
+			transObj = sessionObj.beginTransaction();
+			Incident editIn = (Incident) sessionObj.load(Incident.class, incident.getId());
+			sessionObj.update(editIn);
+			System.out.println("Record Updated");
+		} catch ( HibernateException exceptionObj ) {
+			exceptionObj.printStackTrace();
+		} finally {
+			transObj.commit();
+		}
+//		throw new SavingFailedException("Record not found");
+	}
+
+	@Override
+	public List<Incident> getAllIncidents() {
+		List<Incident> incidentsList = new ArrayList();
+		try{
+			transObj = sessionObj.beginTransaction();
+
+			Query queryObj = sessionObj.createQuery("from Incident");
+
+			incidentsList = queryObj.list();
+
+		} catch ( HibernateException exceptionObj ) {
+			exceptionObj.printStackTrace();
+		} finally {
+			transObj.commit();
+		}
+		return incidentsList;
+	}
+
+	@Override
+	public List<Incident> getRedflagIncidents() {
+		List<Incident> redflagIncidents = new ArrayList<>();
+		try {
+			transObj = sessionObj.beginTransaction();
+
+			Query queryObj = sessionObj.createQuery("from Incident where type = 'RED_FLAG'");
+
+			redflagIncidents = queryObj.list();
+
+
+		} catch (HibernateException exceptionObj) {
+			exceptionObj.printStackTrace();
+		} finally {
+			transObj.commit();
+		}
+		return redflagIncidents;
+	}
+
+	@Override
+	public List<Incident> getInterventionIncidents() {
+		List<Incident> interventionIncidents = new ArrayList<>();
+		try {
+			transObj = sessionObj.beginTransaction();
+
+			Query queryObj = sessionObj.createQuery("from Incident where type = 'INTERVENTION'");
+
+			interventionIncidents = queryObj.list();
+
+		} catch (HibernateException exceptionObj) {
+			exceptionObj.printStackTrace();
+		} finally {
+			transObj.commit();
+		}
+		return interventionIncidents;
+	}
+
+	@Override
+	public int countIncidents() {
+		List<Incident> incidents = new ArrayList<>();
+		incidents = getAllIncidents();
+		return incidents.size();
+	}
+
+	@Override
+	public boolean incidentExists(Incident incident) {
+		List<Incident> incidents = new ArrayList<>();
+		incidents = getAllIncidents();
+
+		String titleOfpassedIncident = incident.getTitle();
+		for(Incident item:incidents){
+			String titleOfIncident = item.getTitle();
+			if(titleOfIncident == titleOfpassedIncident){
+				if(item.getComment() == incident.getComment() && item.getType() == incident.getType()){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public List<Incident> getIncidentOfId(int id) {
+		List<Incident> incidents = new ArrayList<>();
+		try {
+			transObj = sessionObj.beginTransaction();
+
+			Query queryObj = sessionObj.createQuery("from Incident where id = id");
+
+			incidents = queryObj.list();
+
+		} catch (HibernateException exceptionObj) {
+			exceptionObj.printStackTrace();
+		} finally {
+			transObj.commit();
+		}
+
+		if(incidents == null){
+			return null;
+		}else if(incidents.isEmpty()){
+			return null;
+		}else{
+			return incidents;
+		}
+	}
+
+	@Override
+	public void deleteIncident(Incident incident) {
+		if(incident != null){
+			if(incidentExists(incident)){
+				try {
+					transObj = sessionObj.beginTransaction();
+					sessionObj.delete(incident);
+					System.out.println("Incident Record With Title: " + incident.getTitle() + " Is Successfully Deleted From Database");
+
+				} catch (HibernateException exceptionObj) {
+					exceptionObj.printStackTrace();
+				} finally {
+					transObj.commit();
+				}
+			}else{
+				System.out.println("There is no record of this incident");
+			}
+		}else{
+			System.out.println("Please an enter an incident to delete");
+		}
+	}
+
 }
--- src/main/java/org/pahappa/systems/models/Incident.java
+++ src/main/java/org/pahappa/systems/models/Incident.java
@@ -1,4 +1,95 @@
 package org.pahappa.systems.models;
 
-public class Incident {
+import java.io.Serializable;
+import java.util.Date;
+import org.pahappa.systems.enums.*;
+
+import javax.persistence.*;
+
+
+/**
+ * The class {@code Incident} represents an occurrence, condition, 
+ * or situation arising in the course of work that resulted in or could 
+ * have resulted in injuries, illnesses, damage, or fatalities.
+ * 
+ * {@code Incident} should have an id, title, type[Redflag, Intervention], 
+ * status[Draft, Under investigation, Resolved, Rejected], createdOn and comment
+ *
+ */
+@Entity
+@Table(name = "incidents")
+public class Incident implements Serializable {
+    private int id;
+    private String title;
+    private Type  type;
+    private Status status;
+    private Date  createdOn;
+    private String comment;
+	//private int counter;
+
+//	@Column(name = "counter")
+//	public int getCounter() {
+//		return counter;
+//	}
+//
+//	public void setCounter(int counter) {
+//		this.counter = counter;
+//	}
+
+	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "title")
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+        
+        @Enumerated(EnumType.STRING)
+	@Column(name = "type")
+	public Type getType() {
+		return type;
+	}
+	public void setType(Type type) {
+		this.type = type;
+	}
+        
+        @Enumerated(EnumType.STRING)
+	@Column(name = "status")
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+        @Temporal(TemporalType.DATE)
+	@Column(name = "created_on")
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	@Column(name = "comment")
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	@Override
+	public String toString() {
+		return title;
+	}
 }
